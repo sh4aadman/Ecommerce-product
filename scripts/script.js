2a110183b12d9efedf40555ef896b8f1020736f8
sh4aadman <sh4adman@gmail.com>
2025-02-08 23:06:33 +0600

    cart-badge and cart-modal

---
+++ scripts/script.js
@@ -0,0 +1,66 @@
+const cart = document.querySelector("#product-cart");
+
+const modal = document.querySelector("#cart-modal");
+
+const badge = document.querySelector("#count-badge");
+let badgeInt = parseInt(badge.textContent);
+
+const form = document.querySelector(".product-cart");
+
+const count = document.querySelector("#product-quantity");
+let countInt = parseInt(count.value);
+
+const button = document.querySelector(".primary-button");
+
+const incrementBtn = document.querySelector("#increment-button");
+
+const decrementBtn = document.querySelector("#decrement-button");
+
+const checkoutBtn = document.querySelector("#check-out");
+
+cart.addEventListener("click", () => {
+    modal.style.display = "block"
+});
+
+cart.addEventListener("mouseover", () => {
+    modal.style.display = "block";
+    setTimeout(() => {
+        modal.style.display = "none";
+    }, 10000);
+});
+
+cart.addEventListener("mouseout", () => {
+    modal.style.display = "none";
+});
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log("submitted!");
+    console.log(count.value);
+    badgeInt++;
+    badge.textContent = badgeInt;
+    if (badgeInt > 0) {
+        badge.style.display = "block";
+    }
+});
+
+incrementBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    countInt++;
+    count.value = countInt;
+    decrementBtn.disabled = false;
+});
+
+decrementBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    countInt--;
+    if (countInt <= 0) {
+        countInt = 0;
+        decrementBtn.disabled = true;
+    }
+    count.value = countInt;
+});
+
+checkoutBtn.addEventListener("click", () => {
+    location.href = "index.html";
+})
