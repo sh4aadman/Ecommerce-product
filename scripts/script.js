c89cc1443b9eb30a6d1e4e4276adb67ade0f63cf
sh4aadman <sh4adman@gmail.com>
2025-03-10 11:57:57 +0600

    not-revised

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,16 @@
 const cartBadge = document.querySelector("#product-cart");
 
-const cartModal = document.querySelector("#cart-modal");
-
 const countBadge = document.querySelector("#count-badge");
 
+const cartModal = document.querySelector("#cart-modal");
+
+const emptyCart = document.querySelector("#empty-cart");
+
+const cartDetails = document.querySelector(".cart-details");
+
 const productImage = document.querySelector("#main-image");
+
+const productName = document.querySelector("#product-name");
 
 const productLightbox = document.querySelector(".light-box");
 
@@ -30,6 +36,8 @@ const addToCartBtn = document.querySelector("#add-to-cart-button");
 const countIncrementBtn = document.querySelector("#increment-button");
 
 const countDecrementBtn = document.querySelector("#decrement-button");
+
+const removeBtn = document.querySelector(".remove-button");
 
 const checkoutBtn = document.querySelector("#check-out");
 
@@ -81,3 +89,80 @@ nextBtn.addEventListener("click", function () {
   lightboxImage.setAttribute("src", `./images/image-product-${imgInt}.jpg`);
   lightboxImage.setAttribute("alt", `product-${imgInt}`);
 });
+
+const changeQuantity = (x) => {
+  productCount.value = x;
+};
+
+countDecrementBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  let count = productCount.value;
+  let countInt = parseInt(count);
+  countInt--;
+  if (countInt <= 0) {
+    countInt = 0;
+    countDecrementBtn.disabled = true;
+  }
+  productCount.value = countInt;
+});
+
+countIncrementBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  let count = productCount.value;
+  let countInt = parseInt(count);
+  countInt++;
+  productCount.value = countInt;
+});
+
+addToCartBtn.addEventListener("click", function () {
+  Location.href = "index.html";
+  emptyCart.style.display = "none";
+  let countInt = parseInt(productCount.value);
+  let total = 125.0 * countInt;
+  cartDetails.innerHTML = `
+  <figure>
+    <img src="./images/image-product-1-thumbnail.jpg" alt="product-thumbnail">
+  </figure>
+  <section class="cart-product-details">
+    <p>${productName.innerHTML}</p>
+    <p>${countInt} x $125.00</p><span>$${total}.00</span>
+  </section>
+  <button class="remove-button"><i class="fa-solid fa-trash-can"></i></button>
+  `;
+});
+
+cartSubmission.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let badgeInt = parseInt(countBadge.textContent);
+  badgeInt++;
+  countBadge.textContent = badgeInt;
+  if (badgeInt > 0) {
+    countBadge.style.display = "block";
+  }
+});
+
+cartBadge.addEventListener("click", function (event) {
+  event.stopImmediatePropagation();
+  cartModal.style.display = "block";
+  const badgeInt = parseInt(countBadge.textContent);
+  if (badgeInt === 0) {
+    checkoutBtn.style.display = "none";
+  }
+  setTimeout(() => {
+    cartModal.style.display = "none";
+  }, 5000);
+});
+
+if (removeBtn) {
+  removeBtn.addEventListener("click", function () {
+    console.log("clicked");
+  });
+}
+
+// document.firstElementChild.addEventListener(
+//   "click",
+//   function () {
+//     cartModal.style.display = "none";
+//   },
+//   false
+// );
